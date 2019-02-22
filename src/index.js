e0366c519220bd8515e3470bd603c72cc8575ccc
Jesse Zhao <j-zhao@users.noreply.github.com>
2019-02-21 21:40:49 -0800

    Refactored components into subdirectory (#23)
    Refactored JS and style components into a subdirectory, added
    react-bootstrap dependency

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 ReactDOM.render(
-    <Router>
-        <Route path='/' exact component={App} />
-    </Router>,
-    document.getElementById('root'));
+  <Router>
+    <Route path='/' exact component={App} />
+  </Router>,
+  document.getElementById('root'));
 registerServiceWorker();
