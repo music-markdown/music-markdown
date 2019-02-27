b50c1f58b816f545d42efeca0f377e25d246d2e2
Shad Sharma <shadanan@gmail.com>
2019-02-26 21:17:24 -0800

    Sandbox (#29)
    * A sandbox for testing out new features.
    
    This PR adds a route to Sandbox.js which enables testing out features in markdown-it-music by simply importing the appropriate javascript and wrapping it in a JSX. This might evolve into some simple manual tests that exercise various components that can be verified with a visual inspection.
    
    * Capitalize component names
    
    React's components always have a capital first letter, even when they are pure functions.

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,27 @@ import React from 'react';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import MarkdownMusicSourceFetcher from './MarkdownItRender.js';
 import ResponsiveContainer from './ResponsiveContainer.js';
+import Sandbox from './Sandbox.js';
 import './App.scss';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <ResponsiveContainer childElement={homeRouter()} />
+    <ResponsiveContainer childElement={HomeRouter()} />
   );
 };
 
-const homeRouter = () => (
+const HomeRouter = () => (
   <Router>
     <div>
-      <Route exact path="/" component={navigation} />
+      <Route exact path="/" component={Navigation} />
+      <Route path="/sandbox" component={Sandbox} />
       <Route path="/repos/:owner/:repo/contents/:path" component={MarkdownMusicSourceFetcher} />
     </div>
   </Router>
 );
 
-const navigation = () => (
+const Navigation = () => (
   <div>
     <h2>Home</h2>
     <Link to="/repos/music-markdown/almost-in-time/contents/California Dreamin' - The Mamas and the Papas.md">
