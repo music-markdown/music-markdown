e727af48ea815dbcd50220b905026635934ba040
Shad Sharma <shadanan@gmail.com>
2019-03-01 23:35:18 -0800

    Update the sandbox page to include chord library.

--- src/components/Sandbox.js
+++ src/components/Sandbox.js
@@ -1,37 +1,40 @@
 import React from 'react';
 import { renderChordDiagram } from 'markdown-it-music/renderers/chord_diagram';
+import { guitarChordLibrary } from 'markdown-it-music/renderers/chord_library';
 
-function renderEsus4Svg() {
-  return renderChordDiagram({
-    fretOffset: 2,
-    mutes: [
-      { string: 1 },
-      { string: 2 }
-    ],
-    notes: [
-      { string: 3, fret: 4 },
-      { string: 4, fret: 5 }
-    ],
-    barres: [
-      { first: 5, last: 6, fret: 6 }
-    ]
-  }, 200, 200);
-}
-
-const sandbox = () => (
+const Sandbox = () => (
   <div>
     <h1>Music Markdown Sandbox</h1>
     <p>
       This page exercises various subcomponents of music-markdown and
       markdown-it-music.
     </p>
+
     <h2>Chord Diagram Renderer</h2>
     <p>
       This section exercises the Chord Diagram Renderer.
     </p>
     <h3>Esus4 (200x200)</h3>
-    <span dangerouslySetInnerHTML={{ __html: renderEsus4Svg() }} />
+    <div dangerouslySetInnerHTML={
+      { __html: renderChordDiagram('o2 m1 m2 n3,2 n4,4 b5,6,5', 200, 200) }
+    } />
+
+    <h2>Entire Chord Library</h2>
+    <AllChords/>
   </div>
 );
 
-export default sandbox;
+const AllChords = () => (
+  Array.from(guitarChordLibrary.keys()).map((chord, index) => (
+    <div key={index} align="center" style={{ display: 'inline-block' }}>
+      {guitarChordLibrary.get(chord).map((value, index) => (
+        <span key={index} dangerouslySetInnerHTML={
+          { __html: renderChordDiagram(value) }
+        } />
+      ))}
+      <h5>{chord}</h5>
+    </div>
+  ))
+);
+
+export default Sandbox;
