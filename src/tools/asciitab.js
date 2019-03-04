098e265653c0af56a1cc8d8538a26342147dee4b
Shad Sharma <shadanan@gmail.com>
2019-03-03 22:34:44 -0800

    This PR introduces a script that can be used either from the command line or the browser to convert an ASCII Tab file to Markdown Music. There will be a lot of room for improvement on this script. Right now, it recognizes the following:
    Headings such as 'Verse 1:' or '[Verse 1]'
    Verses with a chord line and one or more lyric lines
    
    Fixes #13

---
+++ src/tools/asciitab.js
@@ -0,0 +1,115 @@
+#!/usr/bin/env node
+'use strict';
+
+const Chord = require('markdown-it-music/parsers/chord');
+
+const State = {
+  DEFAULT: 1,
+  VERSE: 2
+};
+
+function getMaybeHeading(line) {
+  if (line.endsWith(':')) {
+    return `## ${line.slice(0, -1)}`;
+  }
+
+  if (line.startsWith('[') && line.endsWith(']')) {
+    return `## ${line.slice(1, -1)}`;
+  }
+
+  return null;
+}
+
+function getMaybeChords(line) {
+  const tokens = line.trim().split(/\s+/);
+
+  for (const token of tokens) {
+    if (!Chord.isChord(token)) {
+      return null;
+    }
+  }
+
+  return `c1: ${line}`;
+}
+
+function convert(input) {
+  let state = State.DEFAULT;
+  let voiceIndex = 1;
+  const lines = input.split(/\n/);
+  const output = [];
+
+  for (const line of lines) {
+    if (state === State.DEFAULT) {
+      const maybeHeading = getMaybeHeading(line);
+      if (maybeHeading) {
+        output.push(maybeHeading);
+        continue;
+      }
+
+      const maybeChords = getMaybeChords(line);
+      if (maybeChords) {
+        state = State.VERSE;
+        output.push('```chords');
+        output.push(maybeChords);
+        voiceIndex = 1;
+        continue;
+      }
+
+      output.push(line);
+      continue;
+    }
+
+    if (state === State.VERSE) {
+      const maybeHeading = getMaybeHeading(line);
+      if (maybeHeading) {
+        state = State.DEFAULT;
+        output.push('```');
+        output.push('');
+        output.push(maybeHeading);
+        continue;
+      }
+
+      const maybeChords = getMaybeChords(line);
+      if (maybeChords) {
+        output.push('');
+        output.push(maybeChords);
+        voiceIndex = 1;
+        continue;
+      }
+
+      if (line.trim() === '') {
+        continue;
+      }
+
+      output.push(`l${voiceIndex}: ${line}`);
+      voiceIndex += 1;
+      continue;
+    }
+  }
+
+  if (state === State.VERSE) {
+    output.push('```');
+  }
+
+  return output.join('\n');
+}
+
+async function read(stream) {
+  let buffer = Buffer.alloc(0);
+  for await (const chunk of stream) {
+    buffer = Buffer.concat([buffer, chunk]);
+  }
+  return buffer.toString('utf8');
+}
+
+async function main() {
+  console.log(convert(await read(process.stdin)));
+}
+
+if (typeof require != 'undefined' && require.main == module) {
+  main();
+}
+
+module.exports = {
+  convert
+};
