7b13d49ca2233930b5df8716f64ffca0d5d25edc
Jesse Zhao <jez@amazon.com>
2019-03-05 20:26:24 -0800

    Adds basic navbar
    Adds basic navbar with placeholder functionality to add repos and list
    repo contents.

--- src/components/ResponsiveContainer.js
+++ src/components/ResponsiveContainer.js
@@ -1,9 +1,15 @@
 import Container from '../../node_modules/react-bootstrap/Container.js';
 import React from 'react';
+import MusicMarkdownNavbar from './MusicMarkdownNavbar.js';
+
+const RESPONSIVE_CONTAINER_KEY = 'app-container';
 
 function ResponsiveContainer(props) {
   return (
-    <Container>{props.childElement}</Container>
+    <Container key={RESPONSIVE_CONTAINER_KEY}>
+      <MusicMarkdownNavbar />
+      {props.children}
+    </Container>
   );
 }
 
