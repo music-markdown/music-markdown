f3abfad639a5a0ac1db3448a5d70e65ca8ed4ffb
Jesse Zhao <jez@amazon.com>
2019-03-05 22:07:09 -0800

    Adds functionality to list repo files

--- src/components/MusicMarkdownNavbar.js
+++ src/components/MusicMarkdownNavbar.js
@@ -3,58 +3,67 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Nav from 'react-bootstrap/Nav';
 import React from 'react';
 
-class MusicMarkdownNavbar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.repoList = [{
-      'owner': 'music-markdown',
-      'repo': 'almost-in-time',
-      'path': '/'
-    }];
-  }
+const darkLightThemeFlag = 'dark';
+const REPO_LIST = 'repoList';
 
-  render() {
-    return this.createNavbar();
+const MusicMarkdownNavbar = () => {
+  if (!localStorage.getItem(REPO_LIST)) {
+    addToRepoList('music-markdown/almost-in-time');
   }
+  console.log(localStorage.getItem(REPO_LIST));
+  return createNavbar();
+};
 
-  createNavbar() {
-    const darkLightThemeFlag = 'dark';
-    return (
-      <Navbar bg={darkLightThemeFlag} expand="lg" variant={darkLightThemeFlag} key="test">
-        <Navbar.Brand href="/">Music Markdown</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse>
-          <Nav className="mr-auto">
-            <NavDropdown title="Music Repositories">
-              {/* TODO: Build edit repo functionality */}
-              <NavDropdown.Item><b>Edit Repositories</b></NavDropdown.Item>
-              <NavDropdown.Divider />
-              {this.getRepositoriesDropdownItems()}
-            </NavDropdown>
-            <Nav.Link href="#/sandbox">Sandbox (Beta)</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
+function createNavbar() {
+  return (
+    <Navbar bg={darkLightThemeFlag} expand="lg" variant={darkLightThemeFlag} key="test">
+      <Navbar.Brand href="/">Music Markdown</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse>
+        <Nav className="mr-auto">
+          <NavDropdown title="Music Repositories">
+            {/* TODO: Build edit repo functionality */}
+            <NavDropdown.Item><b>Edit Repositories</b></NavDropdown.Item>
+            <NavDropdown.Divider />
+            {getRepositoriesDropdownItems()}
+          </NavDropdown>
+          <Nav.Link href="#/sandbox">Sandbox (Beta)</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
 
-  /**
-   * For all added repositories, add it to the dropdown list
-   */
-  // TODO: List first x items, then put in dropdown item to expand full list
-  getRepositoriesDropdownItems() {
-    const repoDropdownItems = [];
-    this.repoList.forEach(function(repo) {
-      const repoId = `${repo.owner}/${repo.repo}${repo.path}`;
-      // TODO: List valid files after clicking on repo name
-      const itemHref = `/repos/${repo.owner}/${repo.repo}/contents${repo.path}`;
-      repoDropdownItems.push(
-        <NavDropdown.Item href={itemHref} key={`dropdown-item-${repoId}`}>
-          {repoId}
-        </NavDropdown.Item>);
-    });
-    return repoDropdownItems;
+function getRepoList() {
+  return localStorage.getItem(REPO_LIST).split(' ');
+}
+
+function addToRepoList(githubRepo) {
+  const repoList = localStorage.getItem(REPO_LIST);
+  if (!localStorage.getItem(REPO_LIST)) {
+    localStorage.setItem(REPO_LIST, githubRepo);
+  } else {
+    localStorage.setItem(REPO_LIST, repoList.concat(` ${githubRepo}`));
   }
+}
+
+/**
+ * For all added repositories, add it to the dropdown list
+ */
+// TODO: List first x items, then put in dropdown item to expand full list
+function getRepositoriesDropdownItems() {
+  const repoDropdownItems = [];
+  const repoList = getRepoList();
+  repoList.forEach(function(repo) {
+    const repoId = `${repo.owner}/${repo.repo}${repo.path}`;
+    // TODO: List valid files after clicking on repo name
+    const itemHref = `/repos/${repo.owner}/${repo.repo}/contents${repo.path}`;
+    repoDropdownItems.push(
+      <NavDropdown.Item href={itemHref} key={`dropdown-item-${repoId}`}>
+        {repoId}
+      </NavDropdown.Item>);
+  });
+  return repoDropdownItems;
 }
 
 export default MusicMarkdownNavbar;
