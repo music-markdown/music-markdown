d212aa98fc0266758cb40236a448ddd197b9b5e8
Shad Sharma <shadanan@gmail.com>
2019-03-10 00:14:16 -0800

    Responsive YouTube Player (#47)
    * Responsive YouTube Player
    
    This PR reads the YAML header from music markdown files and renders a responsive embeded YouTube player for the video.
    
    * Get width of Markdown container (#48)

--- src/components/MarkdownMusic.js
+++ src/components/MarkdownMusic.js
@@ -1,25 +1,35 @@
+import ContainerDimensions from 'react-container-dimensions';
 import React from 'react';
 import MarkdownIt from 'markdown-it';
 import MarkdownItMusic from 'markdown-it-music';
+import { YouTubeToggle } from './YouTube';
 
-class MarkdownMusic extends React.Component {
+class MarkdownMusicRender extends React.Component {
   constructor(props) {
     super(props);
 
-    const musicOpts = {
-      transpose: this.props.transpose
-    };
-
     this.md = new MarkdownIt()
-      .use(MarkdownItMusic, musicOpts);
+      .use(MarkdownItMusic);
   }
 
   render() {
     this.md.setTranspose(this.props.transpose);
+    this.md.setMaxWidth(this.props.width);
+    const html = this.md.render(this.props.source);
+
     return (
-      <span dangerouslySetInnerHTML={{ __html: this.md.render(this.props.source) }} />
+      <div>
+        <YouTubeToggle youTubeId={this.md.meta.youTubeId} />
+        <span dangerouslySetInnerHTML={{ __html: html }} />
+      </div>
     );
   }
 }
+
+const MarkdownMusic = ({ source, transpose }) => (
+  <ContainerDimensions>
+    <MarkdownMusicRender source={source} transpose={transpose} />
+  </ContainerDimensions>
+);
 
 export default MarkdownMusic;
