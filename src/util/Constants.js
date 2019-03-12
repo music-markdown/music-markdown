d2e488bd0a0409a311c3d90a5e80932513dc8a35
Shad Sharma <shadanan@gmail.com>
2019-03-11 22:38:33 -0700

    Enable GitHub Access Token from Local Storage (#50)
    Enable GitHub Access Token from Local Storage

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -1,2 +1,4 @@
 export const MUSIC_MARKDOWN_LOCAL_STORAGE_NAMESPACE = 'music-markdown';
 export const REPOS_LOCAL_STORAGE_KEY = `${MUSIC_MARKDOWN_LOCAL_STORAGE_NAMESPACE}:repositories`;
+export const GITHUB_TOKEN_LOCAL_STORAGE_KEY = `${MUSIC_MARKDOWN_LOCAL_STORAGE_NAMESPACE}:github_token`;
+export const GITHUB_API_URL = 'https://api.github.com';
