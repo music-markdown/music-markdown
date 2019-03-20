6685a9430010665c30af8cd163e11841636b81b6
Vince Lugli <vilugli@microsoft.com>
2019-03-19 17:13:57 -0700

    Add additional test cases for parsing path.

--- src/components/RouterBreadcrumbs.spec.js
+++ src/components/RouterBreadcrumbs.spec.js
@@ -45,4 +45,43 @@ describe('<RouterBreadcrumbs />', () => {
 
     expect(finalItem).toEqual(['path']);
   });
+
+  it('should parse breadcrumbs even if path contains viewName', () => {
+    const wrapper = mount((
+      <MuiThemeProvider theme={style}>
+        <RouterBreadcrumbs pathname={'/repos/owner/repo/browser/master/browser/viewer/editor'} />
+      </MuiThemeProvider>
+    ));
+
+    const breadcrumbs = wrapper.dive().dive();
+
+    const breadcrumbPath = breadcrumbs.find(Link)
+      .map((node) => node.children().text());
+
+    expect(breadcrumbPath.join('/')).toEqual('Home/owner/repo/master/browser/viewer');
+
+    const finalItem = breadcrumbs.find(Typography)
+      .map((node) => node.children().text());
+
+    expect(finalItem).toEqual(['editor']);
+  });
+
+  it('should parse breadcrumbs even if there are multiple /', () => {
+    const wrapper = mount((
+      <MuiThemeProvider theme={style}>
+        <RouterBreadcrumbs pathname={'/repos//owner/repo//browser/master/path///'} />
+      </MuiThemeProvider>
+    ));
+
+    const breadcrumbs = wrapper.dive().dive();
+
+    const breadcrumbPath = breadcrumbs.find(Link)
+      .map((node) => node.children().text());
+
+    expect(breadcrumbPath.join('/')).toEqual('Home/owner/repo/master');
+
+    const finalItem = breadcrumbs.find(Typography)
+      .map((node) => node.children().text());
+
+    expect(finalItem).toEqual(['path']);});
 });
