54aeb1ebc3181514d549307fda3dee0746a60db2
Vince Lugli <vilugli@microsoft.com>
2019-03-22 10:00:07 -0700

    Add Material-UI to Sandbox, remove bootstrap dependencies, theme to abcjs.

--- src/components/MarkdownMusic.js
+++ src/components/MarkdownMusic.js
@@ -1,12 +1,20 @@
-import ContainerDimensions from 'react-container-dimensions';
 import React from 'react';
 import { connect } from 'react-redux';
 import MarkdownIt from 'markdown-it';
 import MarkdownItMusic from 'markdown-it-music';
+import withStyles from '@material-ui/core/styles/withStyles';
+import jss from 'jss';
 
 import { YouTubeToggle } from './YouTube';
 
-class MarkdownMusicRender extends React.Component {
+const styles = (theme) => ({
+  markdownBody: {
+    backgroundColor: theme.palette.background.default,
+    color: theme.palette.text.primary
+  },
+});
+
+class MusicMarkdown extends React.Component {
   constructor(props) {
     super(props);
 
@@ -21,11 +29,18 @@ class MarkdownMusicRender extends React.Component {
     this.md.setMaxWidth(this.props.width);
     const html = this.md.render(this.props.source);
 
+    const { classes, theme } = this.props;
+
+    // Create a style sheet for <path> tag, so that abcjs color will render in dark/light theme.
+    const sheet = jss.createStyleSheet({ path: { fill: theme.palette.text.primary } });
+    sheet.rules.map.path.selectorText = 'path';
+    sheet.attach();
+
     return (
-      <div>
+      <>
         <YouTubeToggle youTubeId={this.md.meta.youTubeId} />
-        <span dangerouslySetInnerHTML={{ __html: html }} />
-      </div>
+        <div dangerouslySetInnerHTML={{ __html: html }} className={classes.markdownBody}/>
+      </>
     );
   }
 }
@@ -35,12 +50,4 @@ function mapStateToProps(state) {
   return { transposeAmount, columnCount, fontSize };
 }
 
-const ConnectMarkdownMusicRender = connect(mapStateToProps)(MarkdownMusicRender);
-
-const MarkdownMusic = ({ source }) => (
-  <ContainerDimensions>
-    <ConnectMarkdownMusicRender source={source} />
-  </ContainerDimensions>
-);
-
-export default MarkdownMusic;
+export default connect(mapStateToProps)(withStyles(styles, { withTheme: true })(MusicMarkdown));
