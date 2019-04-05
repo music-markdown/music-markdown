949c0d6046c4746505d86816dd1828dcc72a86a4
Shad Sharma <shadanan@gmail.com>
2019-04-04 17:33:17 -0700

    Move exit button into the toolbar

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -2,9 +2,12 @@ import { getContents, putContents } from '../lib/github';
 import AceEditor from 'react-ace';
 import CheckIcon from '@material-ui/icons/Check';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import DirectoryBreadcrumbs from './RouterBreadcrumbs';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import Fab from '@material-ui/core/Fab';
 import Grid from '@material-ui/core/Grid';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import { Link } from 'react-router-dom';
 import MusicMarkdown from './MusicMarkdown';
 import Paper from '@material-ui/core/Paper';
 import PhotoFilterIcon from '@material-ui/icons/PhotoFilter';
@@ -115,11 +118,15 @@ class MarkdownEditor extends React.Component {
 
   render = () => {
     const { saving, success, isLoaded, isDirty } = this.state;
-    const { classes, theme } = this.props;
+    const { classes, theme, location } = this.props;
 
     const buttonClassname = classNames({
       [classes.buttonSuccess]: success,
     });
+
+    const parts = location.pathname.split('/');
+    parts[4] = 'viewer';
+    const viewerLink = parts.join('/');
 
     if (!isLoaded) {
       return (
@@ -128,40 +135,46 @@ class MarkdownEditor extends React.Component {
     }
 
     return (
-      <div className={classes.root}>
-        <Grid container spacing={8}>
-          <Grid item xs={12}>
-            <Paper className={classes.toolbar}>
-              <Fab disabled={!isDirty} className={`${buttonClassname} ${classes.fab}`} onClick={this.handleSave}>
-                {success ? <CheckIcon /> : <SaveIcon />}
-                {saving && <CircularProgress size={68} className={classes.fabProgress} />}
-              </Fab>
-              <Fab className={classes.fab} onClick={this.handleAutoFormat}>
-                <PhotoFilterIcon />
-              </Fab>
-            </Paper>
+      <>
+        <DirectoryBreadcrumbs pathname={this.props.location.pathname} />
+        <div className={classes.root}>
+          <Grid container spacing={8}>
+            <Grid item xs={12}>
+              <Paper className={classes.toolbar}>
+                <Fab disabled={!isDirty} className={`${buttonClassname} ${classes.fab}`} onClick={this.handleSave}>
+                  {success ? <CheckIcon /> : <SaveIcon />}
+                  {saving && <CircularProgress size={68} className={classes.fabProgress} />}
+                </Fab>
+                <Fab className={classes.fab} onClick={this.handleAutoFormat}>
+                  <PhotoFilterIcon />
+                </Fab>
+                <Fab component={Link} to={viewerLink} className={classes.fab}>
+                  <ExitToAppIcon />
+                </Fab>
+              </Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper className={classes.paper}>
+                <AceEditor
+                  name="brace-editor"
+                  mode="markdown"
+                  theme={theme.palette.type === 'dark' ? 'monokai' : 'github'}
+                  width="100%"
+                  maxLines={Infinity}
+                  className={classes.editor}
+                  onChange={this.handleChange}
+                  value={this.state.markdown}
+                  editorProps={{ $blockScrolling: true }} />
+              </Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper className={classes.paper}>
+                <MusicMarkdown source={this.state.markdown}></MusicMarkdown>
+              </Paper>
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <Paper className={classes.paper}>
-              <AceEditor
-                name="brace-editor"
-                mode="markdown"
-                theme={theme.palette.type === 'dark' ? 'monokai' : 'github'}
-                width="100%"
-                maxLines={Infinity}
-                className={classes.editor}
-                onChange={this.handleChange}
-                value={this.state.markdown}
-                editorProps={{ $blockScrolling: true }} />
-            </Paper>
-          </Grid>
-          <Grid item xs={6}>
-            <Paper className={classes.paper}>
-              <MusicMarkdown source={this.state.markdown}></MusicMarkdown>
-            </Paper>
-          </Grid>
-        </Grid>
-      </div>
+        </div>
+      </>
     );
   }
 }
