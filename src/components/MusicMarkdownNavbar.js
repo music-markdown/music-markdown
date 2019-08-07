47cca755d7b84123f668b6665ccfb0be30582651
Jesse Zhao <jessezhao1992@gmail.com>
2019-08-06 18:53:36 -0700

    Adds file indexing and path restructure
    Changes the idea of a resource to full path of a file, e.g. /repos/{repo}/{branch}/{file}, and paths for major components (viewer/editor/browser). Uses this as a base for indexing files.

--- src/components/MusicMarkdownNavbar.js
+++ src/components/MusicMarkdownNavbar.js
@@ -1,3 +1,5 @@
+import { REPOS_RESOURCE, REPO_REGEX } from '../lib/constants';
+
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
@@ -9,7 +11,6 @@ import { Link } from 'react-router-dom';
 import MusicToolbar from './MusicToolbar';
 import Paper from '@material-ui/core/Paper';
 import Popper from '@material-ui/core/Popper';
-import { REPO_REGEX } from '../lib/constants';
 import React from 'react';
 import { Route } from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search';
@@ -37,7 +38,7 @@ const styles = (theme) => ({
 
 const BrowseButton = ({ match }) => (
   <Tooltip title="Song List View">
-    <IconButton component={Link} to={`/repos/${match.params.repo}/browser/${match.params.branch}/`}>
+    <IconButton component={Link} to={`/browser/${REPOS_RESOURCE}/${match.params.repo}/browser/${match.params.branch}`}>
       <ViewListIcon />
     </IconButton>
   </Tooltip>
@@ -45,7 +46,7 @@ const BrowseButton = ({ match }) => (
 
 const EditButton = ({ match }) => (
   <Tooltip title="Edit Song">
-    <IconButton component={Link} to={`/repos/${match.params.repo}/editor/${match.params.branch}/${match.params.path}`}>
+    <IconButton component={Link} to={`/editor/${REPOS_RESOURCE}/${match.params.repo}/${match.params.branch}/${match.params.path}`}>
       <EditIcon />
     </IconButton>
   </Tooltip>
@@ -53,7 +54,7 @@ const EditButton = ({ match }) => (
 
 const ViewButton = ({ match }) => (
   <Tooltip title="Markdown View">
-    <IconButton component={Link} to={`/repos/${match.params.repo}/viewer/${match.params.branch}/${match.params.path}`}>
+    <IconButton component={Link} to={`/viewer/${REPOS_RESOURCE}/${match.params.repo}/${match.params.branch}/${match.params.path}`}>
       <ExitToAppIcon />
     </IconButton>
   </Tooltip>
@@ -61,9 +62,11 @@ const ViewButton = ({ match }) => (
 
 // TODO: Placeholder for search functionality
 const SearchToolbar = () => (
-  <IconButton>
-    <SearchIcon />
-  </IconButton>
+  <Tooltip title="Song Search">
+    <IconButton>
+      <SearchIcon />
+    </IconButton>
+  </Tooltip>
 );
 
 class MusicMarkdownNavbar extends React.Component {
@@ -100,16 +103,16 @@ class MusicMarkdownNavbar extends React.Component {
           </Button>
           <div className={classes.grow} />
 
-          <Route path={`${REPO_REGEX}/:mode/:branch/:path*`} component={SearchToolbar} />
-          <Route path={['/sandbox', `${REPO_REGEX}/:mode(viewer|editor)/:branch/:path*`]} component={MusicToolbar} />
-          <Route path={`${REPO_REGEX}/:mode(viewer|editor)/:branch/:path*`} component={BrowseButton} />
-          <Route path={`${REPO_REGEX}/viewer/:branch/:path*`} component={EditButton} />
-          <Route path={`${REPO_REGEX}/editor/:branch/:path*`} component={ViewButton} />
+          <Route path={`/`} component={SearchToolbar} />
+          <Route path={['/sandbox', `/:mode(viewer|editer)/${REPO_REGEX}/:branch/:path*`]} component={MusicToolbar} />
+          <Route path={`/:mode(viewer|editer)/${REPO_REGEX}/:branch/:path*`} component={BrowseButton} />
+          <Route path={`/viewer/${REPO_REGEX}/:branch/:path*`} component={EditButton} />
+          <Route path={`/editer/${REPO_REGEX}/:branch/:path*`} component={ViewButton} />
 
           <IconButton onClick={this.handleSettingsClick} buttonRef={(node) => {
             this.settingsAnchorEl = node;
           }}>
-            <SettingsIcon/>
+            <SettingsIcon />
           </IconButton>
           <Popper
             id='settings-popper'
--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,5 @@
 export const COLUMN_COUNT_QUERY_KEY = 'columns';
-export const LOCAL_STORAGE_NAMESPACE = 'music-markdown';
-export const REPO_REGEX = '/repos/:repo([^/]+/[^/]+)';
+export const WINDOW_STORAGE_NAMESPACE = 'music-markdown';
+export const REPOS_RESOURCE = 'repos';
+export const REPO_REGEX = `${REPOS_RESOURCE}/:repo([^/]+/[^/]+)`;
 export const TRANSPOSE_QUERY_KEY = 'transpose';
