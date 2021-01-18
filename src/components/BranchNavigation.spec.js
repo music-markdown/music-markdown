e2de3f569ba98df3838a4632caa07734f35bc3be
Shad Sharma <shadanan@gmail.com>
2021-01-17 19:17:10 -0800

    Tidy up warnings for BranchNavigation

--- src/components/BranchNavigation.spec.js
+++ src/components/BranchNavigation.spec.js
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router } from "react-router-dom";
 import BranchNavigation from "./BranchNavigation";
 import { mockGetBranchesResponse } from "../lib/MockGithubResponses";
 import { render } from "@testing-library/react";
+import { REPO_REGEX } from "../lib/constants";
 
 describe("BranchNavigation", () => {
   beforeEach(async () => {
@@ -13,7 +14,7 @@ describe("BranchNavigation", () => {
     fetch.mockResponse(JSON.stringify(mockGetBranchesResponse));
     render(
       <Router>
-        <Route path="/" exact component={BranchNavigation} />
+        <Route path={REPO_REGEX} exact component={BranchNavigation} />
       </Router>
     );
   });
