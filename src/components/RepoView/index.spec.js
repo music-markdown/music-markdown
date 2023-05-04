f461231826e0ff7db3aecd0e3a4e291d70115ce7
Shad Sharma <shadanan@gmail.com>
2023-05-03 21:58:56 -0700

    Fix test warnings

--- src/components/RepoView/index.spec.js
+++ src/components/RepoView/index.spec.js
@@ -1,5 +1,5 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { render } from "@testing-library/react";
+import { act, render } from "@testing-library/react";
 import RepoViewer from ".";
 import { GitHubApiProvider } from "../../context/GitHubApiProvider";
 import { ReposProvider } from "../../context/ReposProvider";
@@ -7,16 +7,19 @@ import SnackbarProvider from "../../context/SnackbarProvider";
 
 describe("RepoViewer", () => {
   it("renders without crashing", async () => {
-    render(
-      <GitHubApiProvider>
-        <ReposProvider>
-          <ThemeProvider theme={createTheme()}>
-            <SnackbarProvider>
-              <RepoViewer />
-            </SnackbarProvider>
-          </ThemeProvider>
-        </ReposProvider>
-      </GitHubApiProvider>
-    );
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    await act(async () => {
+      render(
+        <GitHubApiProvider>
+          <ReposProvider>
+            <ThemeProvider theme={createTheme()}>
+              <SnackbarProvider>
+                <RepoViewer />
+              </SnackbarProvider>
+            </ThemeProvider>
+          </ReposProvider>
+        </GitHubApiProvider>
+      );
+    });
   });
 });
