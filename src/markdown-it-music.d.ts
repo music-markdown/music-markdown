6fe304b9d9a43b734678aef43e28b6c28c8ec035
Shad Sharma <shadanan@gmail.com>
2024-01-12 14:42:18 -0800

    Tidy .d.ts files

--- src/markdown-it-music.d.ts
+++ src/markdown-it-music.d.ts
@@ -1,1 +1,7 @@
+declare module "markdown-it-music" {
+  import MarkdownIt from "markdown-it";
+  const markdownItMusic: (md: MarkdownIt) => void;
+  export default markdownItMusic;
+}
+
 declare module "markdown-it-music/lib/chord";
--- src/markdownItMusic.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "markdown-it-music" {
-  import MarkdownIt from "markdown-it";
-  const markdownItMusic: (md: MarkdownIt) => void;
-  export default markdownItMusic;
-}
